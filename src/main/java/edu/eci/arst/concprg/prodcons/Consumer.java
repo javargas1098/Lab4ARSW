201f9167e67cde975e236fba63b8f2972ed928b8
unknown <2124259@labinfo.is.escuelaing.edu.co>
2019-02-07 08:27:35 -0500

    primer punto

---
+++ src/main/java/edu/eci/arst/concprg/prodcons/Consumer.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.arst.concprg.prodcons;
+
+import java.util.Queue;
+
+/**
+ *
+ * @author hcadavid
+ */
+public class Consumer extends Thread {
+
+	private Queue<Integer> queue;
+
+	public Consumer(Queue<Integer> queue) {
+		this.queue = queue;
+	}
+
+	@Override
+	public void run() {
+		while (true) {
+			synchronized (this) {
+				while (queue.size()==0)
+					try {
+						wait();
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} 
+				if (queue.size() > 0) {
+					int elem = queue.poll();
+					System.out.println("Consumer consumes " + elem);
+				}
+				notifyAll();
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}				
+			}
+			
+				
+			
+		}
+	}
+}
---
+++ src/main/java/edu/eci/arst/concprg/prodcons/Producer.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.arst.concprg.prodcons;
+
+import java.util.Queue;
+import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author hcadavid
+ */
+public class Producer extends Thread {
+
+	private Queue<Integer> queue = null;
+
+	private int dataSeed = 0;
+	private Random rand = null;
+	private final long stockLimit;
+	public static Boolean flag = true;
+
+	public Producer(Queue<Integer> queue, long stockLimit) {
+		this.queue = queue;
+		rand = new Random(System.currentTimeMillis());
+		this.stockLimit = stockLimit;
+	}
+
+	@Override
+	public void run() {
+		while (true) {
+			synchronized (this) {
+
+				while (queue.size() == stockLimit) {
+					try {
+						wait();
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+				dataSeed = dataSeed + rand.nextInt(100);
+				System.out.println("Producer added " + dataSeed);
+				queue.add(dataSeed);
+				try {
+					notifyAll();
+					Thread.sleep(1000);
+				} catch (InterruptedException ex) {
+					Logger.getLogger(Producer.class.getName()).log(Level.SEVERE, null, ex);
+				}
+			}
+
+		}
+	}
+}
